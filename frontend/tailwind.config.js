273fb2796d07eaf48c0ca52367f773aeca2df015
lha0 <lhy23456@gmail.com>
2025-03-05 17:44:02 +0900

    Design: global style 적용

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    fontFamily: {
-      pre: ['Pretendard'],
+    center: true,
+    screens: {
+      mobile: "344px", // mobile(344px) 이상
     },
-    theme: {
-      screens: {
-        'xs': '412px',         // 커스텀 브레이크포인트: 412px 이상일 때 적용
-        'max-xs': { 'max': '350px' }, // 커스텀 브레이크포인트: 350px 이하일 때 적용
+    fontFamily: {
+      preLight: ["Pretendard-Light"],
+      preRegular: ["Pretendard-Regular"],
+      preMedium: ["Pretendard-Medium"],
+      preSemiBold: ["Pretendard-SemiBold"],
+      preBold: ["Pretendard-Bold"],
+      preExtraBold: ["Pretendard-ExtraBold"],
+    },
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        offWhite: "#FAF9F6",
+        title: "#202020",
+        longContent: "#3C3C3C",
+        content: "#828282",
+        content2: "#9D9D9D",
+        subContent: "#DDDDDD",
+        primaryLight: "#3E91FF",
+        primary: "#0381FE",
+        primaryDark: "#0072DE",
+        error: "#D44848",
       },
     },
-    extend: {},
   },
   plugins: [],
 };
