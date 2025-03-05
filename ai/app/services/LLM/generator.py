ea7090f37cee23b03f9aea14ac78ba0bf2a21e67
seof622 <seof622@naver.com>
2025-03-05 16:29:02 +0900

    Refactor: 레시피 쿼리 상수 이름 변경 레시피 쿼리에 사용되던 Openai API 관련 상수 이름 변경

--- ai/app/services/LLM/generator.py
+++ ai/app/services/LLM/generator.py
@@ -8,15 +8,15 @@ openai.api_key = settings.OPENAI_API_KEY
 def generate_from_prompt(system_message, user_prompt):
     """프롬프트를 받아 OpenAI API를 통해 응답을 생성하는 기본 함수"""
     response = openai.chat.completions.create(
-        model=settings.OPENAI_MODEL,
+        model=settings.QUERY_OPENAI_MODEL,
         messages=[
             {"role": "system", "content": system_message},
             {"role": "user", "content": user_prompt}
         ],
-        max_tokens=settings.OPENAI_MAX_TOKENS,
-        temperature=settings.OPENAI_TEMPERATURE,
-        top_p=settings.OPENAI_TOP_P,
-        frequency_penalty=settings.OPENAI_FREQUENCY_PENALTY
+        max_tokens=settings.QUERY_OPENAI_MAX_TOKENS,
+        temperature=settings.QUERY_OPENAI_TEMPERATURE,
+        top_p=settings.QUERY_OPENAI_TOP_P,
+        frequency_penalty=settings.QUERY_OPENAI_FREQUENCY_PENALTY
     )
 
     return response.choices[0].message.content.strip()
