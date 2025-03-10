c7c2a81dac2ae4ebcb0d891d486a046075aa14c3
lha0 <lhy23456@gmail.com>
2025-03-10 14:45:57 +0900

    Feat: 비디오 정보 공통 컴포넌트 구현 - time, like, view 타입별로 다른 아이콘, 데이터, 타입이름을 props로 넘겨 공통 컴포넌트 적용 가능 - S12P21S003-62

--- frontend/src/pages/recipeList/components/RecipeCard.tsx
+++ frontend/src/pages/recipeList/components/RecipeCard.tsx
@@ -4,6 +4,7 @@ import IconLike from "@assets/icons/IconLike";
 import IconView from "@assets/icons/IconView";
 import { Video } from "@/types/recipeListTypes";
 import { getYoutubeThumbnailUrl } from "@utils/getYoutubeThumbnailUrl";
+import VideoInfo from "@components/common/VideoInfo";
 
 interface RecipeCardProps {
   video: Video;
@@ -14,7 +15,11 @@ const RecipeCard = ({ video }: RecipeCardProps) => {
   return (
     <div className={`flex justify-center`}>
       <div className="w-[80%] h-[60vh] min-h-[450px] p-3 flex flex-col justify-between bg-white rounded-2xl">
-        <img src={thumbnailUrl} alt={video.title} className="w-full h-[45%] min-h-[200px] rounded-xl object-cover" />
+        <img
+          src={thumbnailUrl}
+          alt={video.title}
+          className="w-full h-[45%] min-h-[200px] rounded-xl object-contain bg-black"
+        />
         <div className="flex justify-between items-center">
           <p className="max-w-[90%] overflow-hidden text-ellipsis whitespace-nowrap font-preSemiBold text-base break-keep">
             {video.title}
@@ -24,21 +29,9 @@ const RecipeCard = ({ video }: RecipeCardProps) => {
           </button>
         </div>
         <div className="flex justify-around items-center">
-          <div className="flex flex-col gap-1 justify-center items-center text-sm">
-            <IconClock width={45} height={45} strokeColor="black" />
-            <p className="font-preBold">{video.duration}</p>
-            <p className="font-preRegular">TIME</p>
-          </div>
-          <div className="flex flex-col gap-1 justify-center items-center text-sm">
-            <IconLike width={45} height={45} strokeColor="black" />
-            <p className="font-preBold">{video.like_count.toLocaleString()}</p>
-            <p className="font-preRegular">LIKE</p>
-          </div>
-          <div className="flex flex-col gap-1 justify-center items-center text-sm">
-            <IconView width={45} height={45} strokeColor="black" />
-            <p className="font-preBold">{video.view_count.toLocaleString()}</p>
-            <p className="font-preRegular">VIEW</p>
-          </div>
+          <VideoInfo IconName={IconClock} InfoData={video.duration} InfoType="TIME" />
+          <VideoInfo IconName={IconLike} InfoData={video.like_count.toLocaleString()} InfoType="LIKE" />
+          <VideoInfo IconName={IconView} InfoData={video.view_count.toLocaleString()} InfoType="VIEW" />
         </div>
         <div className="w-full flex justify-end items-center">
           <button className="p-2 rounded-xl bg-primary font-preSemiBold text-white text-sm">요리하기</button>
