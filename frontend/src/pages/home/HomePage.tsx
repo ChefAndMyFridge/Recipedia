da57732fe0e7c97fc3434286faea893ff2177947
SJLee-0525 <sungjoon25@yonsei.ac.kr>
2025-03-10 16:13:05 +0900

    Refactor: Homepage.tsx 경로 수정

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,6 +1,6 @@
 import HomeHeader from "@pages/home/components/HomeHeader.tsx";
-import HomeIngredients from "./components/HomeIngredients.tsx";
-import HomeFooter from "./components/HomeFooter.tsx";
+import HomeIngredients from "@pages/home/components/HomeIngredients.tsx";
+import HomeFooter from "@pages/home/components/HomeFooter.tsx";
 
 import Modal from "@/components/common/modal/Modal";
 
