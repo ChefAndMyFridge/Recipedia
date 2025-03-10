c5002dcae47196c4795b2ad1dc0bf703e43d5575
SJLee-0525 <sungjoon25@yonsei.ac.kr>
2025-03-10 10:28:56 +0900

    Feat: 공용 input 컴포넌트 구현
    - useEffect를 사용해 onChange로 상위 항목에서 접근
    S12P21S003-46

--- frontend/src/pages/home/TestModal.tsx
+++ frontend/src/pages/home/TestModal.tsx
@@ -1,11 +1,14 @@
 import useModalStore from "@/stores/modalStore.ts";
 
+import Input from "@/components/common/input/Input.tsx";
+
 const TestModal = () => {
   const { closeModal } = useModalStore();
 
   return (
     <div className="flex flex-col justify-between h-[50vh]">
       <h1>Test Modal</h1>
+      <Input label="test" type="date" placeHolder="테스트" />
       <button onClick={closeModal}>Close</button>
     </div>
   );
