4d4979e0c3f99097647f664beb8b503e2ab6bb03
lha0 <lhy23456@gmail.com>
2025-03-11 14:08:49 +0900

    Refactor: Button 컴포넌트 리팩토링 - className 타입 추가

--- frontend/src/pages/detailRecipe/DetailRecipePage.tsx
+++ frontend/src/pages/detailRecipe/DetailRecipePage.tsx
@@ -42,8 +42,8 @@ const DetailRecipePage = () => {
 
       {/* 버튼 컨테이너 */}
       <div className="w-full flex justify-end items-center gap-2">
-        <Button type="cancel" content="요리 취소" width="50" className="px-4" />
-        <Button type="confirm" content="요리 종료" width="50" className="px-4" />
+        <Button type="button" design="cancel" content="요리 취소" width="32" className="px-4" />
+        <Button type="button" design="confirm" content="요리 종료" width="32" className="px-4" />
       </div>
     </section>
   );
