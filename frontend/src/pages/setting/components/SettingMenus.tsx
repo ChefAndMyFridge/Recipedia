64d72044ec933c102b62aa0a25aeefaf02280e90
lha0 <lhy23456@gmail.com>
2025-03-12 17:38:29 +0900

    Refactor: 공통 class 변수화 - 세부 디자인 수정

--- frontend/src/pages/setting/components/SettingMenus.tsx
+++ frontend/src/pages/setting/components/SettingMenus.tsx
@@ -3,27 +3,31 @@ import IconPreference from "@assets/icons/IconPreference";
 import IconHeart from "@assets/icons/IconHeart";
 
 const SettingMenus = () => {
+  const bannerClass = "flex flex-col items-start";
+  const titleClass = "text-lg font-preSemiBold";
+  const descriptionClass = "text-sm font-preRegular break-keep";
+
   return (
-    <div className="w-full flex flex-col items-center gap-4">
-      <div className="w-[80%] h-20 bg-white rounded-lg shadow-md flex items-center px-4 gap-4">
+    <div className="w-full h-[60%] flex flex-col items-center justify-center gap-8">
+      <div className="w-[90%] h-24 bg-white rounded-lg shadow-md flex items-center px-4 gap-4">
         <IconHeart width={30} height={30} />
-        <div className="flex flex-col">
-          <p className="text-lg font-preSemiBold">즐겨찾는 레시피</p>
-          <p className="text-sm font-preRegular">찜해놓은 레시피를 모아볼 수 있어요.</p>
+        <div className={bannerClass}>
+          <p className={titleClass}>즐겨찾는 레시피</p>
+          <p className={descriptionClass}>찜해놓은 레시피를 모아볼 수 있어요.</p>
         </div>
       </div>
-      <div className="w-[80%] h-20 bg-white rounded-lg shadow-md flex items-center px-4 gap-4">
+      <div className="w-[90%] h-24 bg-white rounded-lg shadow-md flex items-center px-4 gap-4">
         <IconHistory width={30} height={30} />
-        <div className="flex flex-col">
-          <p className="text-lg font-preSemiBold">이전 레시피</p>
-          <p className="text-sm font-preRegular">과거에 요리했던 레시피를 모아볼 수 있어요.</p>
+        <div className={bannerClass}>
+          <p className={titleClass}>이전 레시피</p>
+          <p className={descriptionClass}>과거에 요리했던 레시피를 모아볼 수 있어요.</p>
         </div>
       </div>
-      <div className="w-[80%] h-20 bg-white rounded-lg shadow-md flex items-center px-4 gap-4">
+      <div className="w-[90%] h-24 bg-white rounded-lg shadow-md flex items-center  px-4 gap-4">
         <IconPreference width={30} height={30} />
-        <div className="flex flex-col">
-          <p className="text-lg font-preSemiBold">개인 선호</p>
-          <p className="text-sm font-preRegular">재료 선호도 등을 설정할 수 있어요.</p>
+        <div className={bannerClass}>
+          <p className={titleClass}>개인 선호</p>
+          <p className={descriptionClass}>재료 선호도 등을 설정할 수 있어요.</p>
         </div>
       </div>
     </div>
--- frontend/src/pages/setting/components/SettingUserInfo.tsx
+++ frontend/src/pages/setting/components/SettingUserInfo.tsx
@@ -3,7 +3,7 @@ import useUserStore from "@stores/userStore";
 const SettingUserInfo = () => {
   const { currentProfileImg, username } = useUserStore();
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
+    <div className="h-[30%] flex flex-col items-center justify-center gap-4">
       <img src={currentProfileImg} alt="profile" className="w-40 h-40 rounded-full" />
       <p className="text-2xl font-preSemiBold">{username}</p>
     </div>
