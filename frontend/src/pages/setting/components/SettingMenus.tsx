4c62b393e49db8e3575dd012cda0007763c4f052
lha0 <lhy23456@gmail.com>
2025-03-13 17:50:46 +0900

    Feat: 즐겨찾기/이전 레시피 페이지 퍼블리싱 - 동일한 레이아웃이지만, 가져오는 데이터가 다르기 때문에 각 페이지 파일 분리 - 레시피 리스트 내 각 아이템들은 동일한 타입의 데이터를 담기 때문에 MyRecipeItem 공통컴포넌트 생성 - 공통 컴포넌트 내 VideoInfo Row version 생성 S12P21S003-95

--- frontend/src/pages/setting/components/SettingMenus.tsx
+++ frontend/src/pages/setting/components/SettingMenus.tsx
@@ -1,34 +1,42 @@
+import { useNavigate } from "react-router-dom";
 import IconHistory from "@assets/icons/IconHistory";
 import IconPreference from "@assets/icons/IconPreference";
 import IconHeart from "@assets/icons/IconHeart";
 
 const SettingMenus = () => {
-  const bannerClass = "flex flex-col items-start";
-  const titleClass = "text-lg font-preSemiBold";
-  const descriptionClass = "text-sm font-preRegular break-keep";
+  const navigate = useNavigate();
+  const BANNER_CLASS = "flex flex-col items-start";
+  const TITLE_CLASS = "text-lg font-preSemiBold";
+  const DESCRIPTION_CLASS = "text-sm font-preRegular break-keep";
 
   return (
     <div className="w-full h-[60%] flex flex-col items-center justify-center gap-8">
-      <div className="w-[90%] h-24 bg-white rounded-lg shadow-md flex items-center px-4 gap-4">
+      <button
+        className="w-[90%] h-24 bg-white rounded-lg shadow-md flex items-center px-4 gap-4"
+        onClick={() => navigate("/setting/favorite")}
+      >
         <IconHeart width={30} height={30} />
-        <div className={bannerClass}>
-          <p className={titleClass}>즐겨찾는 레시피</p>
-          <p className={descriptionClass}>찜해놓은 레시피를 모아볼 수 있어요.</p>
+        <div className={BANNER_CLASS}>
+          <p className={TITLE_CLASS}>즐겨찾는 레시피</p>
+          <p className={DESCRIPTION_CLASS}>찜해놓은 레시피를 모아볼 수 있어요.</p>
         </div>
-      </div>
-      <div className="w-[90%] h-24 bg-white rounded-lg shadow-md flex items-center px-4 gap-4">
+      </button>
+      <button
+        className="w-[90%] h-24 bg-white rounded-lg shadow-md flex items-center px-4 gap-4"
+        onClick={() => navigate("/setting/history")}
+      >
         <IconHistory width={30} height={30} />
-        <div className={bannerClass}>
-          <p className={titleClass}>이전 레시피</p>
-          <p className={descriptionClass}>과거에 요리했던 레시피를 모아볼 수 있어요.</p>
+        <div className={BANNER_CLASS}>
+          <p className={TITLE_CLASS}>이전 레시피</p>
+          <p className={DESCRIPTION_CLASS}>과거에 요리했던 레시피를 모아볼 수 있어요.</p>
         </div>
-      </div>
+      </button>
       <div className="w-[90%] h-24 bg-white rounded-lg shadow-md flex items-center  px-4 gap-4">
         <IconPreference width={30} height={30} />
-        <div className={bannerClass}>
-          <p className={titleClass}>개인 선호</p>
-          <p className={descriptionClass}>재료 선호도 등을 설정할 수 있어요.</p>
-        </div>
+        <button className={BANNER_CLASS} onClick={() => navigate("/setting/preference")}>
+          <p className={TITLE_CLASS}>개인 선호</p>
+          <p className={DESCRIPTION_CLASS}>재료 선호도 등을 설정할 수 있어요.</p>
+        </button>
       </div>
     </div>
   );
