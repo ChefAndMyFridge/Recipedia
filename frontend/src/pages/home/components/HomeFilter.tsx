739f16ea6133db76337573cf9d2b069f83f4a0d3
SJLee-0525 <sungjoon25@yonsei.ac.kr>
2025-03-14 15:55:00 +0900

    Feat: 필터 관련 스토어 생성
    - 홈 화면에 필터 표시할 영역 생성
    - 필터는 음식 장르(type), 선호 재료(preference), 비선호 재료(dislike)로 나뉨
    
    S12P21S003-99

---
+++ frontend/src/pages/home/components/HomeFilter.tsx
@@ -0,0 +1,9 @@
+const HomeFilter = () => {
+  return (
+    <div className="h-10 bg-gray-200">
+      <p>123123</p>
+    </div>
+  );
+};
+
+export default HomeFilter;
--- frontend/src/stores/ingredientsStore.ts
+++ frontend/src/stores/ingredientsStore.ts
@@ -2,21 +2,28 @@ import { create } from "zustand";
 
 import { INGREDIENTS } from "@/data/INGREDIENTS.ts";
 
-import { Ingredients, SelectedIngredients } from "@/types/ingredientsTypes.ts";
+import { Ingredients, SelectedIngredients, filteredInfomations } from "@/types/ingredientsTypes.ts";
 
 interface IngredientsState {
   // ingredients: 조회된 재료 목록, selectedIngredients: 선택된 재료 목록
   ingredients: Ingredients[];
-  setIngredients: (ingredients: Ingredients[]) => void;
   selectedIngredients: Record<number, SelectedIngredients>; // { ingredientInfoId: selectedCount }
+  filteredInfomations: filteredInfomations;
+  setIngredients: (ingredients: Ingredients[]) => void;
   setSelectedCount: (ingredientInfoId: number, ingredientInfo: SelectedIngredients) => void;
   setClearSelectedIngredients: () => void;
+  setFilteredInfomations: (filterKey: keyof filteredInfomations, filterValue: string) => void;
 }
 
 const useIngredientsStore = create<IngredientsState>((set) => ({
   ingredients: [...INGREDIENTS],
-  setIngredients: (ingredients) => set({ ingredients }),
   selectedIngredients: {},
+  filteredInfomations: {
+    type: [],
+    preference: [],
+    dislike: [],
+  },
+  setIngredients: (ingredients) => set({ ingredients }),
   setSelectedCount: (ingredientInfoId, ingredientInfo) => {
     set((state) => {
       const newSelected = { ...state.selectedIngredients };
@@ -32,6 +39,19 @@ const useIngredientsStore = create<IngredientsState>((set) => ({
     });
   },
   setClearSelectedIngredients: () => set({ selectedIngredients: {} }),
+  setFilteredInfomations: (filterKey, filterValue) => {
+    set((state) => {
+      const newSelected = { ...state.filteredInfomations };
+
+      if (newSelected[filterKey].includes(filterValue)) {
+        newSelected[filterKey] = newSelected[filterKey].filter((item) => item !== filterValue);
+      } else {
+        newSelected[filterKey] = [...newSelected[filterKey], filterValue];
+      }
+
+      return { filteredInfomations: newSelected };
+    });
+  },
 }));
 
 export default useIngredientsStore;
