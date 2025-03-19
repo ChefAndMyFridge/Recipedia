8c9c802d77cb8ed5cf108cce98e5172e3889e281
SJLee-0525 <sungjoon25@yonsei.ac.kr>
2025-03-19 10:53:03 +0900

    Feat: 식재료 입력 시 재료 자동완성 구현
    - 추천 개수에는 제한이 없고, 스크롤이 가능함
    - 현재는 입력 단어가 포함되는 경우를 다 검색함: 토마토 -> 방울 토마토, 토마토
    - 시작하는 단어만 검색하게 할 지 고민 중: 토마토 -> 토마토
    S12P21S003-143

--- frontend/src/pages/storeIngredient/StoreIngredientModal.tsx
+++ frontend/src/pages/storeIngredient/StoreIngredientModal.tsx
@@ -1,13 +1,11 @@
 import ModalHeader from "@components/common/modal/ModalHeader";
 import StoreIngredientForm from "@pages/storeIngredient/components/StoreIngredientForm";
-import LoadingPlayer from "@components/common/loading/LoadingPlayer";
 
 const StoreIngredientModal = () => {
   return (
     <div>
       <ModalHeader title="재료 입고" />
       <StoreIngredientForm />
-      <LoadingPlayer />
     </div>
   );
 };
---
+++ frontend/src/pages/storeIngredient/components/IngredientInput.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+
+import useIngredientsStore from "@stores/ingredientsStore";
+
+import { InputProps } from "@/types/commonProps.ts";
+import { Ingredients } from "@/types/ingredientsTypes.ts";
+
+const Suggestion = ({
+  suggestion,
+  onSelectSuggestion,
+}: {
+  suggestion: Ingredients;
+  onSelectSuggestion: (suggestion: string) => void;
+}) => {
+  return (
+    <li
+      key={suggestion.ingredientInfoId}
+      className="flex justify-start items-center h-12 px-3 py-2 gap-3 cursor-pointer hover:bg-gray-100"
+      onMouseDown={() => onSelectSuggestion(suggestion.name)}
+    >
+      <span className="h-full aspect-[1/1] rounded-xl">
+        <img src={suggestion.imageUrl} alt="no image" className="w-full h-full object-cover rounded-3xl" />
+      </span>
+      <span className="font-preRegular">{suggestion.name}</span>
+    </li>
+  );
+};
+
+const IngredientInput = ({ label, name, type, placeHolder }: InputProps) => {
+  const [inputValue, setInputValue] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
+
+  const { ingredientsInfo } = useIngredientsStore();
+
+  // ver1. 입력한 단어가 포함된 재료명 필터링
+  const filteredSuggestions = ingredientsInfo?.filter((ingredient) =>
+    ingredient.name.toLowerCase().includes(inputValue.toLowerCase())
+  );
+
+  // ver2. 입력한 단어로 시작하는 재료명 필터링
+  // const filteredSuggestions = ingredientsInfo
+  // ?.filter((ingredient) => ingredient.name.toLowerCase().startsWith(inputValue.toLowerCase()));
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSelectSuggestion = (suggestion: string) => {
+    setInputValue(suggestion);
+    setIsFocused(false);
+  };
+
+  return (
+    <div className="relative flex flex-col w-full items-start justify-between gap-2">
+      <label className={`font-preMedium text-[#333] font-semibold text-xs`}>{label}</label>
+      <input
+        name={name}
+        type={type}
+        placeholder={placeHolder}
+        value={inputValue}
+        onChange={handleChange}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        className="w-full px-3 py-1 h-12 rounded-lg border border-subcontent bg-white font-preRegular placeholder:text-gray-400"
+        autoComplete="off"
+      />
+
+      {/* 자동 완성 */}
+      {isFocused && filteredSuggestions.length > 0 && (
+        <ul className="absolute top-full left-0 mt-1 w-full max-h-60 z-10 bg-white border border-gray-300 rounded-lg shadow-md overflow-y-auto">
+          {filteredSuggestions.map((suggestion) => (
+            <Suggestion
+              key={suggestion.ingredientInfoId}
+              suggestion={suggestion}
+              onSelectSuggestion={handleSelectSuggestion}
+            />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default IngredientInput;
