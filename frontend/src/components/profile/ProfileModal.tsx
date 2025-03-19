7075474e1c4a5fd06b34787cbe9b77abc265987d
lha0 <lhy23456@gmail.com>
2025-03-19 17:19:59 +0900

    Fix: 뒤로가기 오류 해결 - 뒤로가기 클릭 시, 모달이 다시 뜨는 문제 해결 S12P21S003-147

--- frontend/src/components/profile/ProfileModal.tsx
+++ frontend/src/components/profile/ProfileModal.tsx
@@ -6,10 +6,15 @@ const ProfileModal = () => {
   const { closeModal, openModal } = useModalStore();
   const navigate = useNavigate();
 
+  const handleNavigate = (path: string) => {
+    closeModal();
+    navigate(path);
+  };
+
   return (
     <div className="flex flex-col py-10 justify-center items-center gap-6 font-preSemiBold text-lg">
       {/* 설정 페이지 이동 */}
-      <button onClick={() => navigate("/setting")}>설정</button>
+      <button onClick={() => handleNavigate("/setting")}>설정</button>
       <hr className="w-[80%]" />
       {/* 프로필 전환 */}
       <button onClick={() => openModal(<ProfileChangeModal />)}>프로필 전환</button>
