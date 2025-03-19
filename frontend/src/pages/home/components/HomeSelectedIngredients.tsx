b3d8d35d17a8bf2eea4892c35c3e447bd1dccf1a
SJLee-0525 <sungjoon25@yonsei.ac.kr>
2025-03-19 11:10:10 +0900

    Feat: 선택한 재료 개수 표시
    - 삭제와 선택 개수 표시 중 뭘 띄울 지 고민 중..

--- frontend/src/pages/home/components/HomeSelectedIngredients.tsx
+++ frontend/src/pages/home/components/HomeSelectedIngredients.tsx
@@ -20,15 +20,9 @@ const HomeSelectedIngredients = () => {
                 alt={ingredient.imageUrl}
                 className="w-full h-full object-cover rounded-3xl"
               />
-              <span className="absolute inset-x-0 bottom-0 h-1/2 bg-gradient-to-t from-black/50 to-transparent rounded-b-3xl" />
-              <p
-                className="absolute bottom-0.5 w-full font-preMedium text-xs text-center text-white"
-                style={{
-                  textShadow: "0.5px 0 black, 0 0.5px black, -0.5px 0 black, 0 -0.5px black",
-                }}
-              >
-                {ingredient.name}
-              </p>
+              <span className="absolute flex justify-center items-center right-0 top-0 bg-orange-500 w-5 h-5 rounded-3xl">
+                <p className="font-preRegular text-white text-xs ">{ingredient.selectedCount}</p>
+              </span>
             </div>
           </div>
         ))}
