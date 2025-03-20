bd2fa5a39c3703e5411bf4b14252fc3ede308e9e
SJLee-0525 <sungjoon25@yonsei.ac.kr>
2025-03-20 15:28:27 +0900

    Fix: 재료 상새 정보 조회 시, 개수가 많으면 화면 밖으로 돌출되던 문제 해결 S12P21S003-150

--- frontend/src/pages/detailIngredient/DetailIngredientModal.tsx
+++ frontend/src/pages/detailIngredient/DetailIngredientModal.tsx
@@ -31,7 +31,7 @@ const DetailIngredientModal = ({ ingredient }: IngredientsProps) => {
 
       {/* 선택된 식재료 상세 목록  */}
       <div className="w-full h-fit px-2">
-        <div className="flex h-[35vh] items-start content-start py-2 px-4 gap-y-5 shrink-0 flex-wrap font-preMedium bg-[#EEE] rounded-xl">
+        <div className="flex h-[35vh] max-h-[35vh] items-start content-start py-2 px-4 gap-y-5 shrink-0 flex-wrap font-preMedium bg-[#EEE] rounded-xl overflow-y-auto">
           {ingredient.ingredients &&
             ingredient.ingredients.map((item) => (
               <DetailIngredientItem
