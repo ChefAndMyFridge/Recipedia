4a17c6ee50838ab5448a91277e06a19fce84b431
lha0 <lhy23456@gmail.com>
2025-03-20 16:54:47 +0900

    Feat: 에러페이지 퍼블리싱

--- frontend/src/components/common/error/ErrorPage.tsx
+++ frontend/src/components/common/error/ErrorPage.tsx
@@ -1,9 +1,27 @@
-//임시 에러 페이지
+import IconError from "@/assets/icons/IconError";
+import Button from "../button/Button";
+import { useNavigate } from "react-router-dom";
+
 const ErrorPage = () => {
+  const navigate = useNavigate();
   return (
-    <section className="flex flex-col gap-3 items-center justify-center h-screen">
-      <h1 className="text-2xl font-preBold">오류가 발생했습니다.</h1>
-      <p className="text-base font-preMedium text-content2">잠시 후, 다시 시도해주세요.</p>
+    <section className="flex flex-col gap-20 items-center justify-around h-screen">
+      <div className="flex flex-col gap-8 items-center justify-center">
+        <IconError width={120} height={120} />
+        <div className="flex flex-col gap-4 items-center justify-center">
+          <h1 className="text-2xl font-preBold">일시적인 오류입니다.</h1>
+          <p className="text-base font-preMedium text-content2">잠시 후에 다시 시도해주세요.</p>
+        </div>
+      </div>
+      <Button
+        type="button"
+        design="confirm"
+        content="홈으로"
+        className="w-[90%] h-10"
+        onAction={() => {
+          navigate("/");
+        }}
+      />
     </section>
   );
 };
