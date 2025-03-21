ecffae779c94cc232a817463e5e963cf6e54e302
SJLee-0525 <sungjoon25@yonsei.ac.kr>
2025-03-21 10:12:34 +0900

    Feat: 냉장 / 냉동 고내 상태 선택 가능한 버튼 제작 - 고내 버튼을 클릭 시 자동으로 재료 api 호출하도록 변경 - 추후 api 완성 시 연결 예정 S12P21S003-152

--- frontend/src/pages/home/components/HomeFilter.tsx
+++ frontend/src/pages/home/components/HomeFilter.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import useIngredientsStore from "@stores/ingredientsStore";
+import { useGetIngredientsList } from "@hooks/useIngredientsHooks";
 
 import HomeExpandFilter from "@pages/home/components/HomeExpandFilter";
 
-import FilterButton from "@components/common/button/FilterButton";
+import Button from "@components/common/button/Button";
 import ArrowUp from "@assets/icons/ArrowUp";
 
 const HomeFilter = () => {
@@ -12,27 +13,38 @@ const HomeFilter = () => {
     useIngredientsStore();
 
   const [isExpand, setIsExpand] = useState<false | "type" | "preference" | "dislike">(false);
+  const [location, setLocation] = useState<"all" | "refrigeration" | "frozen">("all");
+
+  // 재료 목록을 가져오는 API 호출
+  const { refetch } = useGetIngredientsList();
+
+  useEffect(() => {
+    refetch();
+  }, [location]);
 
   return (
     <div className="relative flex justify-between items-center w-full h-10 px-4">
       <div className="flex justify-start items-center gap-2">
-        <FilterButton
-          isSelected={filteredInfomations["type"].length > 0}
-          content="장르"
-          count={filteredInfomations["type"].length}
-          onAction={() => setIsExpand("type")}
+        <Button
+          type="button"
+          design={location === "all" ? "confirm" : "cancel"}
+          content="전체"
+          onAction={() => setLocation("all")}
+          className="px-2 py-1"
         />
-        <FilterButton
-          isSelected={filteredInfomations["preference"].length > 0}
-          content="선호 식단"
-          count={filteredInfomations["preference"].length}
-          onAction={() => setIsExpand("preference")}
+        <Button
+          type="button"
+          design={location === "refrigeration" ? "confirm" : "cancel"}
+          content="냉장실"
+          onAction={() => setLocation("refrigeration")}
+          className="px-2 py-1"
         />
-        <FilterButton
-          isSelected={filteredInfomations["dislike"].length > 0}
-          content="비선호 식단"
-          count={filteredInfomations["dislike"].length}
-          onAction={() => setIsExpand("dislike")}
+        <Button
+          type="button"
+          design={location === "frozen" ? "confirm" : "cancel"}
+          content="냉동실"
+          onAction={() => setLocation("frozen")}
+          className="px-2 py-1"
         />
       </div>
       {isExpand !== false && <ArrowUp width={20} height={20} strokeColor="gray" onClick={() => setIsExpand(false)} />}
