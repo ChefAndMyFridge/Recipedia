c9970f3616a83540b3f40224e35d7b9ae7688a9d
최효재 <hyojae5945@gmail.com>
2025-03-21 14:36:53 +0900

    Feat: DB에 새로운 재료 입고시 ElasticSearch에 인덱스 추가 - 추가된 재료도 검색될 수 있게함 S12P21S003-116

--- backend/src/main/java/com/recipidia/ingredient/document/IngredientDocument.java
+++ backend/src/main/java/com/recipidia/ingredient/document/IngredientDocument.java
@@ -1,12 +1,17 @@
 package com.recipidia.ingredient.document;
 
+import com.recipidia.ingredient.entity.IngredientInfo;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(indexName = "ingredient")
 public class IngredientDocument {
 
@@ -18,4 +23,8 @@ public class IngredientDocument {
 
   @Field(type = FieldType.Text)
   private String imageUrl;
+
+  public static IngredientDocument fromEntity(IngredientInfo ingredientInfo) {
+    return new IngredientDocument(ingredientInfo.getId(), ingredientInfo.getName(), ingredientInfo.getImageUrl());
+  }
 }
