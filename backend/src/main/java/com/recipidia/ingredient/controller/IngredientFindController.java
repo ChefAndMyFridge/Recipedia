99cbc8956dc07f5e60e96caef5a4e30663015d4c
최효재 <hyojae5945@gmail.com>
2025-03-21 10:55:07 +0900

    Docs: ElasticSearch findMatchingIngredient Swagger 추가 S12P21S003-24

--- backend/src/main/java/com/recipidia/ingredient/controller/IngredientFindController.java
+++ backend/src/main/java/com/recipidia/ingredient/controller/IngredientFindController.java
@@ -2,6 +2,12 @@ package com.recipidia.ingredient.controller;
 
 import com.recipidia.ingredient.document.IngredientDocument;
 import com.recipidia.ingredient.service.IngredientDocumentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -17,8 +23,40 @@ public class IngredientFindController {
 
   private final IngredientDocumentService ingredientDocumentService;
 
+  @Operation(
+      summary = "재료 이름 자동완성",
+      parameters = {
+          @Parameter(
+              name = "req",
+              description = "자동완성 검색어 (한글 형태소 단위)",
+              example = "장"
+          )
+      },
+      description = "FIGMA : 선호 재료 필터화면",
+      responses = {
+          @ApiResponse(responseCode = "200", description = "재료 자동완성 결과 완성",
+              content = @Content(schema = @Schema(implementation = IngredientDocument.class),
+                  examples = {
+                      @ExampleObject(
+                          name = "응답 데이터",
+                          value = """
+                              [
+                                   {
+                                       "id": 150,
+                                       "name": "갈치"
+                                   },
+                                   {
+                                       "id": 52,
+                                       "name": "김치"
+                                   }
+                              ]
+                              """
+                      )
+                  }))
+      }
+  )
   @GetMapping("/search")
-  public List<IngredientDocument> findAllIngredient(@RequestParam Map<String, String> searchParam) {
+  public List<IngredientDocument> findMatchingIngredient(@RequestParam Map<String, String> searchParam) {
     return ingredientDocumentService.findByMorpheme(searchParam.getOrDefault("req", ""));
   }
 }
