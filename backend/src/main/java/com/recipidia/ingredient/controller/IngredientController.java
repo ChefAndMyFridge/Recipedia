ba1d213f4495564429faa440f995656ebe4e189e
민경훈 <mgh123gh@naver.com>
2025-03-25 13:07:25 +0900

    Fix: swagger 설정 과정에 추가로 들어간 / 제거
    - swagger 테스트 중 삭제 후 복구하는 과정에 ingredient 뒤에 /가 하나 더 들어가 라우팅이 잘못됐습니다.
    - 다시 제거하였습니다.

--- backend/src/main/java/com/recipidia/ingredient/controller/IngredientController.java
+++ backend/src/main/java/com/recipidia/ingredient/controller/IngredientController.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/ingredient/")
+@RequestMapping("/api/v1/ingredient")
 @RequiredArgsConstructor
 public class IngredientController {
 
