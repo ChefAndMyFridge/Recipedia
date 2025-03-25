82f58bd2482e70db126cbb5f3745a0a71da263f2
최효재 <hyojae5945@gmail.com>
2025-03-25 16:14:15 +0900

    Feat: IngredientInfo, IngredientInfoDto에 빠른/늦은 입고일 필드 추가(earliestExpiration, latestExpiration) S12P21S003-118

--- backend/src/main/java/com/recipidia/ingredient/dto/IngredientInfoDto.java
+++ backend/src/main/java/com/recipidia/ingredient/dto/IngredientInfoDto.java
@@ -3,6 +3,7 @@ package com.recipidia.ingredient.dto;
 import com.recipidia.ingredient.entity.IngredientInfo;
 import jakarta.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
@@ -21,6 +22,8 @@ public class IngredientInfoDto implements Serializable {
   private String name;
   private String imageUrl;
   private int totalCount;
+  private LocalDateTime earliestExpiration;
+  private LocalDateTime latestExpiration;
   private final List<IngredientDto> ingredients = new ArrayList<>();
 
   public IngredientInfoDto(Long id, String name, String imageUrl) {
@@ -30,6 +33,7 @@ public class IngredientInfoDto implements Serializable {
   }
 
   public static IngredientInfoDto fromEntity(IngredientInfo ingredientInfo) {
+    ingredientInfo.setEarliestExpiration();
     IngredientInfoDto ingredientInfoDto = new IngredientInfoDto(
         ingredientInfo.getId(),
         ingredientInfo.getName(),
@@ -39,6 +43,8 @@ public class IngredientInfoDto implements Serializable {
     ingredientInfoDto.totalCount = (int) ingredientInfo.getIngredients().stream()
         .filter(ingredient -> !ingredient.isReleased())
         .count();
+    ingredientInfoDto.earliestExpiration = ingredientInfo.getEarliestExpiration();
+    ingredientInfoDto.latestExpiration = ingredientInfo.getLatestExpiration();
     //ingredientInfoDto.totalCount = ingredientInfoDto.ingredients.size();
     return ingredientInfoDto;
   }
