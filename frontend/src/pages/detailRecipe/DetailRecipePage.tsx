90dbcf6d7a8469f6bd34f2a05252ba2598453e7f
lha0 <lhy23456@gmail.com>
2025-03-25 16:50:12 +0900

    Style: import 띄어쓰기 추가

--- frontend/src/pages/detailRecipe/DetailRecipePage.tsx
+++ frontend/src/pages/detailRecipe/DetailRecipePage.tsx
@@ -1,11 +1,14 @@
 import { useParams } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
+
 import ErrorPage from "@components/common/error/ErrorPage";
 import LoadingPlayer from "@components/common/loading/LoadingPlayer";
+import Modal from "@components/common/modal/Modal";
+
 import { useGetRecipeDetail } from "@hooks/useRecipeHooks";
+
 import DetailRecipeLandscapePage from "@pages/detailRecipe/DetailRecipeLandscapePage";
 import DetailRecipePortraitPage from "@pages/detailRecipe/DetailRecipePortraitPage";
-import { ErrorBoundary } from "react-error-boundary";
-import Modal from "@/components/common/modal/Modal";
 
 const DetailRecipePage = () => {
   //detailRecipe 페이지 진입 시, 해당 레시피 정보 get api 호출 예정
