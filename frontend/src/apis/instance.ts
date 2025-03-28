d831ae95bbfc20d3fa9139258318a29d66d22000
최효재 <hyojae5945@gmail.com>
2025-03-28 15:31:51 +0900

    Chore: baseUrl에 따라 프론트엔드 API URL 동적 설정

--- frontend/src/apis/instance.ts
+++ frontend/src/apis/instance.ts
@@ -2,8 +2,15 @@ import axios from "axios";
 
 const { VITE_API_URL } = import.meta.env;
 
+const base = import.meta.env.BASE_URL;
+
+const baseURL = base === "/master/" 
+  ? "https://j12s003.p.ssafy.io/master/api"
+  : "https://j12s003.p.ssafy.io/api";
+
 const instance = axios.create({
-  baseURL: VITE_API_URL, // 프로덕션 환경
+  // baseURL: VITE_API_URL, // 프로덕션 환경
+  baseURL,
   headers: {
     "Content-Type": "application/json; charset=UTF-8",
     "Access-Control-Allow-Origin": "*",
