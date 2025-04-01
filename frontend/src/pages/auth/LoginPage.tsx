aaf12b6f2901c12710c751d718e27e2a1dd642c7
SJLee-0525 <sungjoon25@yonsei.ac.kr>
2025-04-01 17:29:05 +0900

    Design: 임시 파비콘 추가 - 로그인 페이지에도 파비콘 추가 - 로그인 상태에서 로그인 페이지 접근 시도시 메인으로 리다이렉트 되도록 설정 S12P21S003-230

--- frontend/src/pages/auth/LoginPage.tsx
+++ frontend/src/pages/auth/LoginPage.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import "@pages/auth/auth.css";
+
+import React, { useEffect } from "react";
 
 import { useNavigate } from "react-router-dom";
 
@@ -7,10 +9,21 @@ import { authLoginApi } from "@apis/userApi";
 import Input from "@components/common/input/Input.tsx";
 import Button from "@components/common/button/Button.tsx";
 
-import noImg from "@assets/images/noIngredient/noIngredient.jpg";
+import logo from "@assets/images/favicon/favicon.png";
 
 const Login = () => {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    checkLoginStatus();
+  }, []);
+
+  function checkLoginStatus() {
+    const token = localStorage.getItem("jwt");
+    if (token) {
+      navigate("/"); // 이미 로그인된 상태라면 홈으로 리다이렉트
+    }
+  }
 
   async function handleLogin(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -35,15 +48,18 @@ const Login = () => {
   }
 
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center gap-8">
-      <div className="w-48 h-fit flex flex-col items-center justify-center">
+    <div className="w-full h-full flex flex-col items-center justify-center gap-12">
+      <div className="w-48 h-fit flex flex-col items-center justify-center slide-down">
         {/* 추후 파비콘 이미지 사용 예정 */}
-        <img src={noImg} alt="" className="w-48 h-48 aspect-[1/1] object-cover" />
+        <img src={logo} alt="" className="w-48 h-48 aspect-[1/1] object-cover" />
         <h1 className="text-2xl font-bold font-preBold">RECIPEDIA</h1>
       </div>
 
-      <form onSubmit={handleLogin} className="flex flex-col items-center justify-center w-3/4 gap-4">
-        <div className="flex flex-col items-center justify-center w-full gap-2">
+      <form
+        onSubmit={handleLogin}
+        className="flex flex-col items-center justify-center w-4/5 gap-6 p-5 bg-white shadow-md rounded-xl"
+      >
+        <div className="flex flex-col items-center justify-center w-full gap-4">
           <Input label="아이디" name="username" type="text" placeHolder="아이디를 입력해주세요." />
           <Input label="비밀번호" name="password" type="password" placeHolder="비밀번호를 입력해주세요." />
         </div>
