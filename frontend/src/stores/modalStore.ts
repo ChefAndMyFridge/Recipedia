6a2cdb7951c6a73da39df46bbb151c5b511e5d9f
SJLee-0525 <sungjoon25@yonsei.ac.kr>
2025-04-02 10:22:43 +0900

    Refactor: 모달 애니메이션 확대 - 모달이 닫힐 때, 올라가는 애니메이션 추가 - 0.3초의 지연 후 실제로 닫히고, 그 동안 올라가는 애니메이션 재생 - 혹시라도 그 사이에 모달 여는 동작이 있는 것을 대비해, 닫히는 중이면 모달이 새로 열리지 않게끔 함 - 모달 블러 추가 S12P21S003-237

--- frontend/src/stores/modalStore.ts
+++ frontend/src/stores/modalStore.ts
@@ -2,8 +2,9 @@ import React from "react";
 import { create } from "zustand";
 
 interface ModalState {
-  // isOpen: 모달이 열려있는지 여부, modalContent: 모달에 렌더링할 컴포넌트트, openModal: 모달 열기 함수, closeModal: 모달 닫기 함수
+  // isOpen: 모달이 열려있는지 여부, isClosing: 모달이 닫히는 중인지 여부, modalContent: 모달에 렌더링할 컴포넌트트, openModal: 모달 열기 함수, closeModal: 모달 닫기 함수
   isOpen: boolean;
+  isClosing: boolean;
   modalContent: React.ReactNode | null;
   openModal: (content: React.ReactNode) => void;
   closeModal: () => void;
@@ -11,9 +12,16 @@ interface ModalState {
 
 const useModalStore = create<ModalState>((set) => ({
   isOpen: false,
+  isClosing: false,
   modalContent: null,
   openModal: (content) => set({ isOpen: true, modalContent: content }),
-  closeModal: () => set({ isOpen: false, modalContent: null }),
+  closeModal: () => {
+    set({ isClosing: true });
+
+    setTimeout(() => {
+      set({ isClosing: false, isOpen: false, modalContent: null });
+    }, 300);
+  },
 }));
 
 export default useModalStore;
