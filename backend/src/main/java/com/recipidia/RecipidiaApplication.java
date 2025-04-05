6fc44d0429c431ebc085b40e891f0381ae948bcd
최효재 <hyojae5945@gmail.com>
2025-04-05 19:38:45 +0900

    Remove: 필요없는 라인, 파일 삭제

--- backend/src/main/java/com/recipidia/RecipidiaApplication.java
+++ backend/src/main/java/com/recipidia/RecipidiaApplication.java
@@ -12,9 +12,4 @@ public class RecipidiaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RecipidiaApplication.class, args);
 	}
-
-	@PostConstruct
-	public void showDbUrl() {
-		System.out.println("DB 접속 URL: " + System.getenv("MYSQL_HOST"));
-	}
 }
